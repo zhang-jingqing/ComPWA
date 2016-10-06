a7ad86cecc08b68567c038b4fc63c17910d5acca
stefzke <spfluege@gmail.com>
2016-10-06 16:14:02 +0200

    changed helicity fit app ui so it can be used on himster

--- test/HelicityAmplitudeFitApp.cpp
+++ test/HelicityAmplitudeFitApp.cpp
@@ -82,243 +82,262 @@ using Estimator::MinLogLH::MinLogLH;
  * The main function.
  */
 int main(int argc, char **argv) {
-  int seed = 1234;
-  string input_config_file("Physics/HelicityAmplitude/JPSI_ypipi.xml");
-  string input_data_file_path("3Part-4vecs.root");
-  string input_phsp_file_path("3Part-4vecs.root");
-  string output_filename("FitResultJPSI.root");
-  string output_directory("./");
-  string output_file_suffix("");
-  if (argc > 1)
-    input_config_file = argv[1];
-  if (argc > 2)
-    input_data_file_path = argv[2];
-  if (argc > 3)
-    input_phsp_file_path = argv[3];
-  if (argc > 4)
-    output_filename = argv[4];
-  if (argc > 5)
-    output_directory = argv[5];
-  if (argc > 6)
-    seed = atoi(argv[6]);
-  if (argc > 7)
-    output_file_suffix = argv[7];
+  // we expect at least 1 arguments
+  if (argc > 1) {
+    int seed = 1234;
+    boost::filesystem::path input_config_file(argv[1]);
+    int num_events(-1);
+    boost::filesystem::path input_data_file_path("data.root");
+    boost::filesystem::path input_phsp_file_path("phspdata.root");
 
-  Logging log("log", boost::log::trivial::debug);    //initialize logging
+    boost::filesystem::path output_directory = boost::filesystem::absolute(
+        input_config_file, boost::filesystem::current_path());
+    output_directory = output_directory.parent_path();
 
-  BOOST_LOG_TRIVIAL(info)<< "  ComPWA Copyright (C) 2013  Mathias Michel ";
-  BOOST_LOG_TRIVIAL(info)<< "  This program comes with ABSOLUTELY NO WARRANTY; for details see license.txt";
-  BOOST_LOG_TRIVIAL(info)<< std::endl;
+    boost::filesystem::path output_filename_template("fit_result.xml");
 
-  bool useFctTree = true, resultGen = true;
+    string output_file_suffix("");
+    if (argc > 2)
+      num_events = std::atoi(argv[2]);
+    if (argc > 3)
+      input_data_file_path = argv[3];
+    if (argc > 4)
+      input_phsp_file_path = argv[4];
+    if (argc > 5)
+      output_directory = argv[5];
+    if (argc > 6)
+      output_filename_template = argv[6];
+    if (argc > 7)
+      seed = atoi(argv[7]);
+    if (argc > 8)
+      output_file_suffix = argv[8];
 
-  BOOST_LOG_TRIVIAL(info)<< "Load Modules";
+    Logging log("log", boost::log::trivial::debug);    //initialize logging
 
-  ComPWA::Physics::HelicityFormalism::HelicityKinematics* kin =
-      dynamic_cast<ComPWA::Physics::HelicityFormalism::HelicityKinematics*>(ComPWA::Physics::HelicityFormalism::HelicityKinematics::createInstance());
+    BOOST_LOG_TRIVIAL(info)<< "  ComPWA Copyright (C) 2013  Mathias Michel ";
+    BOOST_LOG_TRIVIAL(info)<< "  This program comes with ABSOLUTELY NO WARRANTY; for details see license.txt";
+    BOOST_LOG_TRIVIAL(info)<< std::endl;
 
-  ComPWA::Physics::DecayTree::DecayConfiguration decay_configuration;
-  ComPWA::Physics::DecayTree::DecayXMLConfigReader xml_reader(
-      decay_configuration);
-  xml_reader.readConfig(input_config_file);
+    bool useFctTree = true, resultGen = true;
 
-  ComPWA::Physics::DecayTree::DecayTreeFactory decay_tree_factory(
-      decay_configuration);
+    BOOST_LOG_TRIVIAL(info)<< "Load Modules";
 
-  std::vector<ComPWA::Physics::DecayTree::DecayTree> decay_trees =
-      decay_tree_factory.createDecayTrees();
+    ComPWA::Physics::HelicityFormalism::HelicityKinematics* kin =
+        dynamic_cast<ComPWA::Physics::HelicityFormalism::HelicityKinematics*>(ComPWA::Physics::HelicityFormalism::HelicityKinematics::createInstance());
 
-  std::cout << "created " << decay_trees.size() << " decay trees from "
-      << input_config_file << " config file!" << std::endl;
+    ComPWA::Physics::DecayTree::DecayConfiguration decay_configuration;
+    ComPWA::Physics::DecayTree::DecayXMLConfigReader xml_reader(
+        decay_configuration);
+    xml_reader.readConfig(input_config_file.string());
 
-  if (decay_trees.size() > 0) {
-    ComPWA::Physics::HelicityFormalism::TopologyAmplitudeFactory topology_amp_factory;
+    ComPWA::Physics::DecayTree::DecayTreeFactory decay_tree_factory(
+        decay_configuration);
 
-    Event dummy_event = topology_amp_factory.createDummyEvent(decay_trees[0]);
+    std::vector<ComPWA::Physics::DecayTree::DecayTree> decay_trees =
+        decay_tree_factory.createDecayTrees();
 
-    std::vector<ComPWA::Physics::DecayTree::DecayNode> leaves =
-        decay_trees[0].getLeaves();
-    std::vector<ComPWA::Physics::HelicityFormalism::ParticleStateInfo> fs_particles;
-    for (auto iter = leaves.begin(); iter != leaves.end(); ++iter) {
-      fs_particles.push_back(iter->state_info_);
-    }
+    std::cout << "created " << decay_trees.size() << " decay trees from "
+        << input_config_file << " config file!" << std::endl;
 
-    ComPWA::Physics::HelicityFormalism::FinalStateParticleCombinatorics fsp_combinatorics;
-    fsp_combinatorics.init(fs_particles, dummy_event);
+    if (decay_trees.size() > 0) {
+      ComPWA::Physics::HelicityFormalism::TopologyAmplitudeFactory topology_amp_factory;
 
-    ComPWA::Physics::HelicityFormalism::HelicityKinematics* kinematics =
-        (ComPWA::Physics::HelicityFormalism::HelicityKinematics*) ComPWA::Physics::HelicityFormalism::HelicityKinematics::createInstance();
+      Event dummy_event = topology_amp_factory.createDummyEvent(decay_trees[0]);
 
-    std::vector<ComPWA::Physics::HelicityFormalism::TwoBodyDecayTopology> decay_topologies =
-        topology_amp_factory.generateDecayTopologies(decay_trees);
+      std::vector<ComPWA::Physics::DecayTree::DecayNode> leaves =
+          decay_trees[0].getLeaves();
+      std::vector<ComPWA::Physics::HelicityFormalism::ParticleStateInfo> fs_particles;
+      for (auto iter = leaves.begin(); iter != leaves.end(); ++iter) {
+        fs_particles.push_back(iter->state_info_);
+      }
 
-    kinematics->setDecayTopologies(decay_topologies);
-    kinematics->init(fsp_combinatorics);
+      ComPWA::Physics::HelicityFormalism::FinalStateParticleCombinatorics fsp_combinatorics;
+      fsp_combinatorics.init(fs_particles, dummy_event);
 
-    // The helicity amplitude tree factory sorts the decay trees based
-    // on their topology, and then creates the list of amplitude trees from the
-    // topology grouped decay trees
+      ComPWA::Physics::HelicityFormalism::HelicityKinematics* kinematics =
+          (ComPWA::Physics::HelicityFormalism::HelicityKinematics*) ComPWA::Physics::HelicityFormalism::HelicityKinematics::createInstance();
 
-    std::vector<ComPWA::Physics::HelicityFormalism::TopologyAmplitude> topology_amplitudes =
-        topology_amp_factory.generateTopologyAmplitudes(decay_trees);
+      std::vector<ComPWA::Physics::HelicityFormalism::TwoBodyDecayTopology> decay_topologies =
+          topology_amp_factory.generateDecayTopologies(decay_trees);
 
-    std::cout << "created " << topology_amplitudes.size()
-        << " topology amplitudes from the decay trees!" << std::endl;
+      kinematics->setDecayTopologies(decay_topologies);
+      kinematics->init(fsp_combinatorics);
 
-    std::shared_ptr<ComPWA::Physics::HelicityFormalism::CoherentAmplitude> amp(
-        new ComPWA::Physics::HelicityFormalism::CoherentAmplitude(
-            topology_amplitudes));
+      // The helicity amplitude tree factory sorts the decay trees based
+      // on their topology, and then creates the list of amplitude trees from the
+      // topology grouped decay trees
 
-    std::shared_ptr<RootReader> myReader(
-        new RootReader(input_data_file_path, "events", 3000));
-    myReader->resetWeights();    //setting weights to 1
-    std::shared_ptr<RootReader> myPHSPReader(
-        new RootReader(input_phsp_file_path, "events", 30000));
-    myPHSPReader->setEfficiency(shared_ptr<Efficiency>(new UnitEfficiency()));    //setting efficiency to 1
+      std::vector<ComPWA::Physics::HelicityFormalism::TopologyAmplitude> topology_amplitudes =
+          topology_amp_factory.generateTopologyAmplitudes(decay_trees);
 
-    if (myReader) {
-      Event tmp;
-      unsigned int num_events(myReader->getNEvents());
-      if (num_events > 0) {
-        tmp = myReader->getEvent(0);
-        tmp.reorderEvent(dummy_event);
-        DataPointStorage::Instance().layoutDataStorageStructure(1, num_events,
-            tmp);
-        progressBar bar(num_events);
-        for (unsigned int i = 0; i < num_events; ++i) {
-          tmp = myReader->getEvent(i);
+      std::cout << "created " << topology_amplitudes.size()
+          << " topology amplitudes from the decay trees!" << std::endl;
+
+      std::shared_ptr<ComPWA::Physics::HelicityFormalism::CoherentAmplitude> amp(
+          new ComPWA::Physics::HelicityFormalism::CoherentAmplitude(
+              topology_amplitudes));
+
+      std::shared_ptr<RootReader> myReader(
+          new RootReader(input_data_file_path.string(), "events", num_events));
+      myReader->resetWeights();    //setting weights to 1
+      int phsp_events(num_events * 10);
+      if (num_events == -1)
+        phsp_events = -1;
+      std::shared_ptr<RootReader> myPHSPReader(
+          new RootReader(input_phsp_file_path.string(), "events", phsp_events));
+      myPHSPReader->setEfficiency(shared_ptr<Efficiency>(new UnitEfficiency()));    //setting efficiency to 1
+
+      if (myReader) {
+        Event tmp;
+        unsigned int num_events(myReader->getNEvents());
+        if (num_events > 0) {
+          tmp = myReader->getEvent(0);
           tmp.reorderEvent(dummy_event);
-          DataPointStorage::Instance().addEvent(1, tmp);
-          bar.nextEvent();
+          DataPointStorage::Instance().layoutDataStorageStructure(1, num_events,
+              tmp);
+          progressBar bar(num_events);
+          for (unsigned int i = 0; i < num_events; ++i) {
+            tmp = myReader->getEvent(i);
+            tmp.reorderEvent(dummy_event);
+            DataPointStorage::Instance().addEvent(1, tmp);
+            bar.nextEvent();
+          }
         }
       }
-    }
-    if (myPHSPReader) {
-      Event tmp;
-      unsigned int num_events(myPHSPReader->getNEvents());
-      if (num_events > 0) {
-        tmp = myPHSPReader->getEvent(0);
-        tmp.reorderEvent(dummy_event);
-        DataPointStorage::Instance().layoutDataStorageStructure(0, num_events,
-            tmp);
-        progressBar bar(num_events);
-        for (unsigned int i = 0; i < num_events; ++i) {
-          tmp = myPHSPReader->getEvent(i);
+      if (myPHSPReader) {
+        Event tmp;
+        unsigned int num_events(myPHSPReader->getNEvents());
+        if (num_events > 0) {
+          tmp = myPHSPReader->getEvent(0);
           tmp.reorderEvent(dummy_event);
-          DataPointStorage::Instance().addEvent(0, tmp);
-          bar.nextEvent();
+          DataPointStorage::Instance().layoutDataStorageStructure(0, num_events,
+              tmp);
+          progressBar bar(num_events);
+          for (unsigned int i = 0; i < num_events; ++i) {
+            tmp = myPHSPReader->getEvent(i);
+            tmp.reorderEvent(dummy_event);
+            DataPointStorage::Instance().addEvent(0, tmp);
+            bar.nextEvent();
+          }
         }
       }
-    }
 
-    ParameterList par;
-    std::shared_ptr<Optimizer::ControlParameter> esti;
-    amp->copyParameterList(par);    //perfect startvalues
+      ParameterList par;
+      std::shared_ptr<Optimizer::ControlParameter> esti;
+      amp->copyParameterList(par);    //perfect startvalues
 
-    std::cout << "number of parameters: " << par.GetNDouble() << std::endl;
-    for (auto const& param : par.GetDoubleParameters()) {
-      std::cout << param->GetName() << " " << param->IsFixed() << std::endl;
-    }
-
-    esti = MinLogLH::createInstance(amp, myReader, myPHSPReader, 0,
-        myReader->getNEvents());
-    MinLogLH* contrPar = dynamic_cast<MinLogLH*>(&*(esti->Instance()));
-    std::shared_ptr<FunctionTree> tree;
-    if (useFctTree) {
-      contrPar->setUseFunctionTree(1);
-      tree = contrPar->getTree();
-    }
-
-    std::shared_ptr<Optimizer::Optimizer> opti(
-        new Optimizer::Minuit2::MinuitIF(esti, par));
-
-    ParameterList test;
-
-    BOOST_LOG_TRIVIAL(info)<< "LH with optimal parameters: " << esti->controlParameter(par);
-    if (useFctTree)
-      BOOST_LOG_TRIVIAL(info)<<tree;
-    double startInt[par.GetNDouble()], optiInt[par.GetNDouble()];
-    TRandom3 rand(seed);
-    for (unsigned int i = 0; i < par.GetNDouble(); i++) {
-      std::shared_ptr<DoubleParameter> tmp = par.GetDoubleParameter(i);
-      optiInt[i] = tmp->GetValue();
-      if (!tmp->IsFixed()) {
-        BOOST_LOG_TRIVIAL(debug)<< *tmp;
-        tmp->SetValue(rand.Uniform(tmp->GetValue()*0.8, tmp->GetValue()*1.2));
-        tmp->SetError(tmp->GetValue());
-        if (!tmp->GetValue())
-        tmp->SetError(1.);
+      std::cout << "number of parameters: " << par.GetNDouble() << std::endl;
+      for (auto const& param : par.GetDoubleParameters()) {
+        std::cout << param->GetName() << " " << param->IsFixed() << std::endl;
       }
-      startInt[i] = tmp->GetValue();
+
+      esti = MinLogLH::createInstance(amp, myReader, myPHSPReader, 0,
+          myReader->getNEvents());
+      MinLogLH* contrPar = dynamic_cast<MinLogLH*>(&*(esti->Instance()));
+      std::shared_ptr<FunctionTree> tree;
+      if (useFctTree) {
+        contrPar->setUseFunctionTree(1);
+        tree = contrPar->getTree();
+      }
+
+      std::shared_ptr<Optimizer::Optimizer> opti(
+          new Optimizer::Minuit2::MinuitIF(esti, par));
+
+      ParameterList test;
+
+      BOOST_LOG_TRIVIAL(info)<< "LH with optimal parameters: " << esti->controlParameter(par);
+      if (useFctTree)
+        BOOST_LOG_TRIVIAL(info)<<tree;
+      double startInt[par.GetNDouble()], optiInt[par.GetNDouble()];
+      TRandom3 rand(seed);
+      for (unsigned int i = 0; i < par.GetNDouble(); i++) {
+        std::shared_ptr<DoubleParameter> tmp = par.GetDoubleParameter(i);
+        optiInt[i] = tmp->GetValue();
+        if (!tmp->IsFixed()) {
+          BOOST_LOG_TRIVIAL(debug)<< *tmp;
+          tmp->SetValue(rand.Uniform(tmp->GetValue()*0.8, tmp->GetValue()*1.2));
+          tmp->SetError(tmp->GetValue());
+          if (!tmp->GetValue())
+          tmp->SetError(1.);
+        }
+        startInt[i] = tmp->GetValue();
+      }
+      BOOST_LOG_TRIVIAL(info)<< "LH with following parameters: " << esti->controlParameter(par);
+      for (unsigned int i = 0; i < par.GetNDouble(); i++) {
+        BOOST_LOG_TRIVIAL(info)<< par.GetDoubleParameter(i)->GetName() << " = " << par.GetDoubleParameter(i)->GetValue();
+      }
+
+      BOOST_LOG_TRIVIAL(info)<< "Start Fit";
+      std::shared_ptr<FitResult> genResult = opti->exec(par);
+      BOOST_LOG_TRIVIAL(info)<< "Final LH = " << genResult->getResult();
+
+      BOOST_LOG_TRIVIAL(info)<< "Optimierte intensitäten: " << esti->controlParameter(par);
+      for (unsigned int i = 0; i < par.GetNDouble(); i++) {
+        BOOST_LOG_TRIVIAL(info)<< par.GetDoubleParameter(i)->GetName() << " = " << par.GetDoubleParameter(i)->GetValue()
+        << "   [ start: " << startInt[i] << " ," << " optimal: " << optiInt[i] << " ]";
+      }
+
+      // create weighted phase-space sample
+      unsigned int dataSize = 100000;
+
+      /*std::shared_ptr<DataReader::Data> plotdata(
+       new DataReader::RootReader::RootReader());
+       std::shared_ptr<Generator> gen(
+       new DataReader::RootGenerator::RootGenerator());
+
+       progressBar bar(dataSize);
+       for (unsigned int i = 0; i < dataSize; i++) {
+       if (i > 0)
+       i--;
+       Event tmp;
+       gen->generate(tmp);
+       double ampRnd = gen->getUniform();
+       if (ampRnd > tmp.getWeight())
+       continue;
+       dataPoint point(tmp);
+       ParameterList list;
+       list = amp->intensity(point);    //unfortunatly not thread safe
+       tmp.setWeight(*list.GetDoubleParameter(0));    //reset weight
+       tmp.setEfficiency(1.);
+       i++;
+       plotdata->pushEvent(tmp);    //unfortunatly not thread safe
+       bar.nextEvent();
+       }*/
+
+      std::shared_ptr<DataReader::Data> plotdata(new RootReader());
+      std::shared_ptr<Generator> gen(
+          new DataReader::RootGenerator::RootGenerator(seed));
+
+      RunManager run(amp, gen);
+      run.setGenerator(gen);
+      run.setData(plotdata);
+      run.generate(dataSize);
+
+      // create output filenames
+      string output_data_filename(output_filename_template.string());
+      if (output_file_suffix.compare("") != 0) {
+        // add output suffix
+        std::stringstream ss;
+        ss << output_filename_template.stem().string() << "_"
+            << output_file_suffix
+            << output_filename_template.extension().string();
+        output_data_filename = ss.str();
+      }
+
+      string output_gendata_path = output_directory.string() + "/resultdata.root";
+      if (output_file_suffix != "") {
+        output_gendata_path = output_directory.string() + "/resultdata_"
+            + output_file_suffix + ".root";
+      }
+
+      plotdata->writeData(output_gendata_path, "events");
+      genResult->writeXML(output_directory.string() + "/" + output_data_filename);
+
+      if (!resultGen)
+        return 0;
+
+      BOOST_LOG_TRIVIAL(info)<< "Done";
     }
-    BOOST_LOG_TRIVIAL(info)<< "LH with following parameters: " << esti->controlParameter(par);
-    for (unsigned int i = 0; i < par.GetNDouble(); i++) {
-      BOOST_LOG_TRIVIAL(info)<< par.GetDoubleParameter(i)->GetName() << " = " << par.GetDoubleParameter(i)->GetValue();
-    }
-
-    BOOST_LOG_TRIVIAL(info)<< "Start Fit";
-    std::shared_ptr<FitResult> genResult = opti->exec(par);
-    BOOST_LOG_TRIVIAL(info)<< "Final LH = " << genResult->getResult();
-
-    BOOST_LOG_TRIVIAL(info)<< "Optimierte intensitäten: " << esti->controlParameter(par);
-    for (unsigned int i = 0; i < par.GetNDouble(); i++) {
-      BOOST_LOG_TRIVIAL(info)<< par.GetDoubleParameter(i)->GetName() << " = " << par.GetDoubleParameter(i)->GetValue()
-      << "   [ start: " << startInt[i] << " ," << " optimal: " << optiInt[i] << " ]";
-    }
-
-    // create weighted phase-space sample
-    unsigned int dataSize = 100000;
-
-    /*std::shared_ptr<DataReader::Data> plotdata(
-     new DataReader::RootReader::RootReader());
-     std::shared_ptr<Generator> gen(
-     new DataReader::RootGenerator::RootGenerator());
-
-     progressBar bar(dataSize);
-     for (unsigned int i = 0; i < dataSize; i++) {
-     if (i > 0)
-     i--;
-     Event tmp;
-     gen->generate(tmp);
-     double ampRnd = gen->getUniform();
-     if (ampRnd > tmp.getWeight())
-     continue;
-     dataPoint point(tmp);
-     ParameterList list;
-     list = amp->intensity(point);    //unfortunatly not thread safe
-     tmp.setWeight(*list.GetDoubleParameter(0));    //reset weight
-     tmp.setEfficiency(1.);
-     i++;
-     plotdata->pushEvent(tmp);    //unfortunatly not thread safe
-     bar.nextEvent();
-     }*/
-
-    std::shared_ptr<DataReader::Data> plotdata(new RootReader());
-    std::shared_ptr<Generator> gen(
-        new DataReader::RootGenerator::RootGenerator(seed));
-
-    RunManager run(amp, gen);
-    run.setGenerator(gen);
-    run.setData(plotdata);
-    run.generate(dataSize);
-
-    string output_gendata_path = output_directory + "/resultdata.root";
-    string output_file_path = output_directory + "/fitresult.xml";
-    if (output_file_suffix != "") {
-      output_gendata_path = output_directory + "/resultdata_"
-          + output_file_suffix + ".root";
-      output_file_path = output_directory + "/fitresult_" + output_file_suffix
-          + ".xml";
-    }
-
-    plotdata->writeData(output_gendata_path, "events");
-    genResult->writeXML(output_file_path);
-
-    if (!resultGen)
-      return 0;
-
-    BOOST_LOG_TRIVIAL(info)<< "Done";
   }
-
   return 0;
 }
